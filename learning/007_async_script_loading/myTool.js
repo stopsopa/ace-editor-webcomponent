c29650ecd255881e3ed6e092f2acb9da2d7954d9
Szymon Dzialowski <szymon.dzialowski@gmail.com>
2025-10-20 02:22:33 +0100

    feat: add async script loading lesson (007)

---
+++ learning/007_async_script_loading/myTool.js
@@ -0,0 +1,7 @@
+export function myTool(el) {
+  el.classList.add("my-tool-class");
+
+  const span = document.createElement("span");
+  span.textContent = "My Tool is Loaded";
+  el.appendChild(span);
+}
